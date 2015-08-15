01be94c1bf2c926997c3fa83702ec2b7c3492326
Ubuntu <ubuntu@ip-172-31-25-145.ap-southeast-1.compute.internal>
2015-08-15 16:36:29 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express')  
+  , app = express()
+  , server = require('http').createServer(app);
+
+  server.listen(3000);
+
+  app.get('/', function(req, res) {  
+		  res.send("Hello prod!");
+		  });
